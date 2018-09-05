b79e55580844cdf946b38ff3276c792a9eae8649
Don Mendelson <donmendelson@gmail.com>
2018-09-05 08:59:01 -0500

    Update project version to 1.0.0 snapshot

--- conga-client/src/test/java/io/fixprotocol/conga/client/TraderTest.java
+++ conga-client/src/test/java/io/fixprotocol/conga/client/TraderTest.java
@@ -139,8 +139,7 @@ public class TraderTest {
   @Test
   public void quiescent() throws Exception {
     Thread.sleep(10000);
-    // System.out.println(trader.toString());
-    trader.terminate();
+    trader.suspend();
     Thread.sleep(2000);
     trader.open();
     Thread.sleep(2000);
